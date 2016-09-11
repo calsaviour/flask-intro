c4c17bcc035358ba5f859692d51209cd4d746f64
Calvin Low <calsaviour@gmail.com>
2016-09-11 16:20:51 +0800

    setup local postgresql

--- settings.sh
+++ settings.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 . venv/bin/activate
-export DATABASE_URL="sqlite:///posts.db"
+#export DATABASE_URL="sqlite:///posts.db"
+export DATABASE_URL="postgresql://localhost/discover_flask_dev"
 export APP_SETTINGS="config.DevelopmentConfig"
